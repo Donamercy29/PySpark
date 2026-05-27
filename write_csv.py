de628a1567c71fcc7b02f5c34ce1d48d7c2b8e9b
Donamercy <donamercy29@gmail.com>
2026-05-27 19:02:26 +0530

    Write Files

---
+++ write_csv.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+spark = SparkSession.builder.appName("Schema3").getOrCreate()
+schema = StructType([
+    StructField("name", StringType(), True),
+    StructField("id", IntegerType(), True),
+    StructField("dept", StringType(), True)
+]
+)
+data = [
+    ("James", 111, "HR"),
+    ("Michael", 222, "IT"),
+    ("Robert", 333, "SALES"),
+    ("Maria", 444, "IT"),
+    ("Jen", 555, "HR")
+]
+
+df = spark.createDataFrame(data, schema)
+df.write.mode("overwrite").option("header", True).csv("output")
